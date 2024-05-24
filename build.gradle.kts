50b1068602d52658d01ccc0fd7be8df63f524910
JUNSEO <djawnstj44@naver.com>
2024-05-24 17:25:43 +0900

    의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+
+    val kotlinVersion = "1.9.24"
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
 }
 
 group = "com.djawnstj"
@@ -19,17 +33,55 @@ repositories {
 }
 
 dependencies {
+    // Spring Web
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // 시큐리티
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // DB
+    runtimeOnly("com.h2database:h2")
+
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Kotlin JDSL
+    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.0.0")
+    implementation("com.linecorp.kotlin-jdsl:jpql-render:3.4.1")
+    implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.0.0")
+
+    // 로깅
+    implementation("io.github.oshai:kotlin-logging-jvm:6.0.9")
+
+    // 코루틴
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.8.1")
+
+    // 테스트
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    // thymeleaf
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
     // websocket
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     // STOMP
-    implementation(group= "org.webjars", name= "stomp-websocket", version= "2.3.3-1")
+    implementation(group= "org.webjars", name= "stomp-websocket", version= "2.3.4")
 }
 
 tasks.withType<KotlinCompile> {
